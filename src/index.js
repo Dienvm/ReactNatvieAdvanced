0556b1ac13d8230f95d3b349a5d3471ebe59038e
Dienvm <dienvm.pnv@gmail.com>
2021-09-25 22:57:28 +0700

    Step 2: Setup eslint, prettier and change vscode config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+const DemoView = () => {
+  return (
+    <View>
+      <Text>demo</Text>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default DemoView;
